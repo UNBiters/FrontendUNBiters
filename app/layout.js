6ce0f4468f4c3d3aef6428426bedc54e80849ae2
anlopezgu <anlopezgu@unal.edu.co>
2023-09-28 01:42:15 -0500

    Creacion Front Login

--- app/layout.js
+++ app/layout.js
@@ -13,8 +13,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <NavbarHome />
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}><NavbarHome />{children}</body>
+      
     </html>
   )
 }
--- app/unbiters/login/page.js
+++ app/unbiters/login/page.js
@@ -1,6 +1,53 @@
+'use client'
+import React from 'react';
+import '/styles/login.css';
+import { Button } from 'flowbite-react';
 
-export default function Login() {
+
+const Login = () => {
     return (
-        <><h1>Login</h1></>
-    )
+        <div>
+            <div className="popup">
+            <img className='logoUnbiters' src='/images/LogoUnb.png'></img>
+                <div className='dataTC'>Al continuar aceptas los terminos y
+                    condiciones y aceptas nuestra politica de tratamiento de datos
+                </div>
+                <div className='emailContainer'>
+                    <input className='emailInput' placeholder='Email' 
+                    /></div>
+
+                <div className='passwordContainer'>
+                    <input type='password' className='passwordInput' placeholder='Contraseña'/>
+                </div>
+                <div className='forgotPassword'>Olvidaste tu contraseña?</div>
+                <Button className='loginBtn'>
+                    <div className='loginText'>Iniciar Sesión
+                    </div>
+                </Button>
+
+                <div className='lineL'></div>
+                <div className='o'>o</div>
+                <div className='lineR'></div>
+
+                <Button className='googleLogin'>
+                    <img className='googleIcon' src='/images/GoogleLogo.png'></img>
+                    <div className='googleText'>Inicia sesión con Google</div>
+                </Button>
+                <Button className='xLogin'>
+                    <img className='xIcon' src='/images/XLogo.png'></img>
+                    <div className='xText'>Inicia sesión con X
+                    </div>
+                </Button>
+
+                <div className='line'></div>
+                <div className='noAccount '>¿No tienes cuenta?</div>
+                <div className='SingIn'>Registarte</div>
+            </div>
+            
+
+
+        </div>
+    );
 }
+
+export default Login;
