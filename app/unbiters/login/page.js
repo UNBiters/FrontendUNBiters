e2f2553c1b44c66e79436d9b568bbe966668cc7a
anlopezgu <anlopezgu@unal.edu.co>
2023-10-09 15:07:10 -0500

    Agustes input login

--- app/unbiters/login/page.js
+++ app/unbiters/login/page.js
@@ -11,15 +11,12 @@ export default function Login() {
 
                         <a href="/unbiters/t&c" className="block mt-1 text-md leading-tight font-medium text-black hover:underline">Al continuar aceptas los terminos y
                             condiciones y aceptas nuestra politica de tratamiento de datos</a>
-                        <div >
-                            <input type="email" id="email" className="w-full mt-2 mb-4 shadow-sm bg-[#F5F5F5] border border-gray-300 text-gray-900 text-bg rounded-lg focus:ring-primary-500 focus:border-primary-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light " placeholder="Email" required />
+
+                        <input type="email" id="email" className="w-full mt-2 mb-4 shadow-sm bg-[#F5F5F5] border border-gray-300 text-gray-900 text-bg rounded-lg focus:ring-primary-500 focus:border-primary-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light " placeholder="Email" required />
+                        <input type="password" id="s" className="w-full mb-4 shadow-sm bg-[#F5F5F5] border border-gray-300 text-gray-900 text-md rounded-lg focus:ring-primary-500 focus:border-primary-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="Contraseña" required />
 
 
-
-                            <input type="password" id="s" className="w-full mb-4 shadow-sm bg-[#F5F5F5] border border-gray-300 text-gray-900 text-md rounded-lg focus:ring-primary-500 focus:border-primary-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="Contraseña" required />
-
-                        </div>
-
+                        <a href="#" className='text-sm mb-3 block ml-auto'>¿Olvido su contraseña?</a>
 
                         <Button href='/' className='boton-lg px-5 mx-1 hover:bg-sky-700 text-balck shadow-xl'>
                             Iniciar Sesión
