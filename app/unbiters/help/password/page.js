7b8c3a317d028ef24fdcb9e31cad83e2606ff827
anlopezgu <anlopezgu@unal.edu.co>
2023-10-18 18:04:12 -0500

    Correciones contacto y recuperar contraseña

--- app/unbiters/help/password/page.js
+++ app/unbiters/help/password/page.js
@@ -1,19 +1,20 @@
 'use client'
 import { Button } from 'flowbite-react';
 import React, { useState } from 'react';
+import client from "@/config/client";
 import axios from 'axios';
 
 
 export default function ForgotPassword() {
-    const [email, setEmail] = useState('');
+    const [correo, setEmail] = useState('');
 
     const handleSubmit = async (e) => {
         e.preventDefault();
 
         try {
-            const response = await axios.post("https://backend-un-biters-git-main-unbiters.vercel.app/api/v1/", { email }); //ruta 
-
-            if (response.status === 200) {
+            const response = await client.post('users/forgotPassword', { correo });
+            console.log(response)
+            if (response.data.status === 'success') {
                 alert('Correo enviado con éxito'); 
             }
         } catch (error) {
@@ -34,11 +35,11 @@ export default function ForgotPassword() {
                     <form onSubmit={handleSubmit}>
                             <input
                                 type="email"
-                                id="email"
+                                id="correo"
                                 className="w-full mt-5 mb-4 shadow-sm bg-[#F5F5F5] border border-gray-300 text-gray-900 text-bg rounded-lg focus:ring-primary-500 focus:border-primary-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light "
                                 placeholder="Email"
                                 required
-                                value={email}
+                                value={correo}
                                 onChange={(e) => setEmail(e.target.value)}
                             />
 
@@ -49,9 +50,9 @@ export default function ForgotPassword() {
 
                     
                     <hr className="border-t border-red-500 border-2 my-6" style={{ width: '90%' }} />
-                    <div class="flex flex-row">
-                        <div class="basis-32">¿No tienes cuenta?</div>
-                        <div class="basis-10">
+                    <div className="flex flex-row">
+                        <div className="basis-32">¿No tienes cuenta?</div>
+                        <div className="basis-10">
                             <a href="#" className="block mt-1 text-md leading-tight font-bold text-black hover:underline">
                                 Registrate
                             </a>
