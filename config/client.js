aca202d066a806518a36a1e3ddf4d1f565425724
Riaño John <jhon091199@gmail.com>
2023-10-22 03:19:40 -0500

    fix client vercel

--- config/client.js
+++ config/client.js
@@ -5,7 +5,7 @@ const agent = new https.Agent({
   rejectUnauthorized: false,
 })
 const client = axios.create({
-  baseURL: "http://127.0.0.1:3000/api/v1/",//"https://backend-un-biters.vercel.app/api/v1/",//"https://backend-un-biters-git-main-unbiters.vercel.app/api/v1/",// para ejecutar en local sin superbase"http://localhost:3005/api",
+  baseURL: "https://backend-un-biters.vercel.app/api/v1/",//"https://backend-un-biters.vercel.app/api/v1/",//"https://backend-un-biters-git-main-unbiters.vercel.app/api/v1/",// para ejecutar en local sin superbase"http://localhost:3005/api",
   //httpsAgent: agent,
   headers: {
     'Content-Type': 'application/json'
@@ -13,7 +13,7 @@ const client = axios.create({
 
 });
 export const myClient = {
-  url: "http://127.0.0.1:3000/api/v1/"
+  url: "https://backend-un-biters.vercel.app/api/v1/"
 }
 
 export default client;
